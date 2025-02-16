d8221cd68f5105d3a8405e945df7fec5d0b766cc
Aditya Reddy <aditya@gmail.com>
2025-02-16 14:22:11 +0530

    react_router

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Movie } from "./pages/Movie";
+import { AppLayout } from "./components/layout/AppLayout";
+import { ErrorPage } from "./pages/ErrorPage";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      errorElement: <ErrorPage/>,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+        {
+          path: "/Movie",
+          element: <Movie />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default App;
